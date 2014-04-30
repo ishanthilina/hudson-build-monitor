2b7c5c42dad27edff916848507a1d4e55d091556
Ishan Thilina Somasiri <ishan@ishans.info>
2014-04-30 22:23:51 +0530

    Created a method to get perform an APIcall to the hudson API

--- Program.cs
+++ Program.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.IO;
+using System.Net;
+
+using RestSharp;
+
+
+
 namespace hudson_build_monitor
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Check_Last_Build();
+        }
+
+        private static void Check_Last_Build()
+        {
+            var client = new RestClient("http://deadlock.netbeans.org/job/trunk/");
+            var request = new RestRequest("lastBuild/api/json", Method.GET);
+            var queryResult = client.Execute(request).Content;
+
+            Console.WriteLine(queryResult);
+            Console.Read();
+            
+
+
         }
     }
 }
