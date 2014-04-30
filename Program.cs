6e0e7c64906d28a0bdafa3f24efd1c852cddbb97
Ishan Thilina Somasiri <ishan+github@ishans.info>
2014-04-30 21:09:12 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hudson_build_monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
