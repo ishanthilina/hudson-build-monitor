5d6c363ca3f3744718fdcfdce17de459436eb9a5
Ishan Thilina Somasiri <ishan@ishans.info>
2014-05-02 12:26:42 +0530

    Switched to the xml API

--- HudsonMonitor.cs
+++ HudsonMonitor.cs
@@ -10,6 +10,10 @@ using Newtonsoft.Json;
 using System.Threading;
 using WMPLib;
 
+using System.Xml;
+using System.IO;
+
+
 namespace hudson_build_monitor
 {
     class HudsonMonitor
@@ -18,13 +22,13 @@ namespace hudson_build_monitor
         public const String SUCCESS = "SUCCESS";
 
         public static Boolean isMonitoringEnabled = false;
-        
+
 
         //settings
         private static String apiRoot = ConfigurationManager.AppSettings["apiRoot"];
-        private static int sleepLength = Convert.ToInt32(ConfigurationManager.AppSettings["monitoringFrequency"])*1000;
+        private static int sleepLength = Convert.ToInt32(ConfigurationManager.AppSettings["monitoringFrequency"]) * 1000;
 
-        
+
 
         public static void run()
         {
@@ -32,8 +36,8 @@ namespace hudson_build_monitor
             {
                 Build lastBuild = Check_Last_Build();
 
-                Console.WriteLine("Last Build Number:" + lastBuild.buildNo);
-                Console.WriteLine("Last Build Status:" + lastBuild.buildStatus);
+                Console.WriteLine("Build Number:" + lastBuild.buildNo + " || Status: " + lastBuild.buildStatus);
+                
                 //if the status is not success, play the alarm
                 if (!lastBuild.buildStatus.Equals(SUCCESS))
                 {
@@ -46,27 +50,50 @@ namespace hudson_build_monitor
             AlarmPlayer.Stop_Alarm();
 
 
-            
+
         }
         /***
          * Checks the status of the last build
-         */ 
+         */
         private static Build Check_Last_Build()
         {
             //get the result from the API
             var client = new RestClient(apiRoot);
-            var request = new RestRequest("lastBuild/api/json", Method.GET);
+            var request = new RestRequest("lastBuild/api/xml", Method.GET);
             var queryResult = client.Execute(request).Content;
 
             //parse the JSON string
-            dynamic result = JsonConvert.DeserializeObject(queryResult);
-            int buildNo = Convert.ToInt32(result.number);
-            String buildStatus = result.result;
-            return new Build(buildNo, buildStatus);
+            //dynamic result = JsonConvert.DeserializeObject(queryResult);
+
+            StringBuilder output = new StringBuilder();
+            String buildStatus;
+            int buildNo;
+
+            XmlReader reader = XmlReader.Create(new StringReader(queryResult));
+            
+                
+                reader.ReadToFollowing("building");
+                String isBuilding = reader.ReadElementContentAsString();
+                //if no build is in progress
+                if (!isBuilding.Equals("true"))
+                {
+                    reader.ReadToFollowing("number");
+                    buildNo = Convert.ToInt32(reader.ReadElementContentAsString());
+                    //reset the reader
+                    reader = XmlReader.Create(new StringReader(queryResult));
+                    
+                    reader.ReadToFollowing("result");
+                    buildStatus = reader.ReadElementContentAsString();
+                    
+                    return new Build(buildNo, buildStatus);
+                }
+
+                else
+                {
+                    return new Build(00, "SUCCESS");
+                }
+                
+
         }
-
-        
-
-
     }
 }
